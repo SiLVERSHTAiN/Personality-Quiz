1ca2b63975460b5b3e4deb99d84e5953abc1223f
ALEKSANDER <57324920+SiLVERSHTAiN@users.noreply.github.com>
2022-11-08 18:04:31 +0700

    Update ResultViewController.swift

--- Personality Quiz/Controller/ResultViewController.swift
+++ Personality Quiz/Controller/ResultViewController.swift
@@ -9,4 +9,33 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    var response: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        updateResult()
+    }
+    
+    private func updateResult() {
+        var frequencyOfAnimals: [AnimalsType: Int] = [:]
+        let animals = response.map { $0.type }
+        
+        for animal in animals {
+            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequensyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequensyAnimal)
+    }
+    
+    private func updateUI(with animal: AnimalsType) {
+        resultAnswerLabel.text = "Вы - \(animal.rawValue)"
+        resultDefinitionLabel.text = "\(animal.definition)"
+    }
 }
